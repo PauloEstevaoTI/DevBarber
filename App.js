fb3c1576a05c32537f3bd1ade6d254e5e1a7ed89
Bombeta <paullo_jf@hotmail.com>
2023-01-10 23:22:52 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+const App = () => {
+  return(
+    <View>
+      <Text>TESTANDO 123</Text>
+    </View>
+
+  )
+}
+
+export default App;
